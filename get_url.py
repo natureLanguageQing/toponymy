0ac5a232810cb7f62d71eec600f0d192f304b41d
624576166@qq.com <19950920ygq>
2020-05-04 00:11:33 +0800

    数据频次统计

--- get_url.py
+++ get_url.py
@@ -1,3 +1,5 @@
+import random
+
 import pandas as pd
 import json
 
@@ -5,18 +7,41 @@ urls = pd.read_excel("附件2：疫情通报网址.xlsx").values.tolist()
 import requests
 import re
 
+headers_pool = [{
+    'User-Agent': 'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.1; Trident/5.0)'},
+]
 data_list = []
-data_dict = {}
-
+word_encoding = []
+ISO = []
 for i in urls:
-    wb_data = requests.get(i[0])
-    print(i[0])
-    wb_data.encoding = 'utf-8'
-    print(wb_data.text)
-    data_dict["url"] = i[0]
-    data_list.append(data_dict)
-    pat = re.compile('>(.*?)<')
-    data_dict["context"] = ''.join(pat.findall(wb_data.text))
+    data_dict = {}
 
-with open('data_context.json', 'w', encoding='utf-8') as f:
+    wb_data = requests.get(i[0], headers=random.choice(headers_pool))
+    print(i[0])
+    print("wb_data.encoding", wb_data.encoding)
+    if wb_data.encoding not in word_encoding:
+        word_encoding.append(wb_data.encoding)
+    if wb_data.encoding == "ISO-8859-1":
+        data_dict["encoding"] = wb_data.encoding
+
+        wb_data.encoding = 'utf-8'
+        data_dict["url"] = i[0]
+        pat = re.compile('>(.*?)<')
+        data_dict["context"] = (pat.findall(wb_data.text))
+        ISO.append(data_dict)
+    else:
+        data_dict["encoding"] = wb_data.encoding
+
+        wb_data.encoding = 'utf-8'
+        data_dict["url"] = i[0]
+        pat = re.compile('>(.*?)<')
+        data_dict["context"] = (pat.findall(wb_data.text))
+        data_list.append(data_dict)
+
+with open('data_context_list.json', 'w', encoding='utf-8') as f:
     f.write(json.dumps(data_list, ensure_ascii=False))
+with open('data_context_ISO.json', 'w', encoding='utf-8') as f:
+    f.write(json.dumps(ISO, ensure_ascii=False))
+import pandas as pd
+
+pd.DataFrame(word_encoding).to_csv("word_encoding.csv")
---
+++ word_count.py
@@ -0,0 +1,19 @@
+import json
+
+with open("data_context_list.json", encoding="utf-8") as f:
+    words = json.load(f)
+word_count = {}
+for index, i in enumerate(words):
+    print(index, i)
+    for j in i["context"]:
+        word = j.strip()
+        if j in word_count.keys():
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+export = []
+for i, j in word_count.items():
+    export.append([i, j])
+import pandas as pd
+
+pd.DataFrame(export).to_csv("export.csv", index=False)
